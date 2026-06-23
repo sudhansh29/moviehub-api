7278f242215c2f1501b2d47ecb6724889d21b81c
sudhansh29 <138241661+sudhansh29@users.noreply.github.com>
2026-06-23 23:25:11 +0530

    app.py

--- app.py
+++ app.py
@@ -1,27 +1,15 @@
 from fastapi import FastAPI
-from sqlalchemy import create_engine, text
 
 app = FastAPI()
 
-DATABASE_URL = "postgresql://postgres:Sudh123@localhost:5432/postgres"
-
-engine = create_engine(DATABASE_URL)
-
 @app.get("/")
 def home():
-    return {"message": "Welcome to MovieHub API"}
+    return {"message": "MovieHub API Running"}
 
 @app.get("/movies")
-def get_movies():
-
-    with engine.connect() as conn:
-
-        result = conn.execute(text("""
-            SELECT *
-            FROM movies
-            ORDER BY id
-        """))
-
-        movies = [dict(row._mapping) for row in result]
-
-    return movies
+def movies():
+    return [
+        {"id": 1, "title": "Interstellar"},
+        {"id": 2, "title": "Inception"},
+        {"id": 3, "title": "RRR"}
+    ]
