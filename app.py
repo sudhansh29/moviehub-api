665994b7071cbcb5078176a548177cda695a6f53
sudhansh29 <138241661+sudhansh29@users.noreply.github.com>
2026-06-25 00:46:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI
 from sqlalchemy import create_engine, text
 import os
+from pydantic import BaseModel
 
 app = FastAPI()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
+
+class Movie(BaseModel):
+    title: str
+    genre: str
 
 @app.get("/")
 def home():
@@ -28,3 +33,21 @@ def get_movies():
         movies = [dict(row._mapping) for row in result]
 
     return movies
+
+@app.post("/movies")
+def add_movie(movie: Movie):
+
+    with engine.begin() as conn:
+
+        conn.execute(
+            text("""
+                INSERT INTO movies(title, genre)
+                VALUES (:title, :genre)
+            """),
+            {
+                "title": movie.title,
+                "genre": movie.genre
+            }
+        )
+
+    return {"message": "Movie added successfully"}
