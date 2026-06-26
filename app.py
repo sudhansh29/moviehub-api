f866cd1e7d6388ada176afe28d248364554ea451
sudhansh29 <138241661+sudhansh29@users.noreply.github.com>
2026-06-26 13:44:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -96,3 +96,21 @@ def update_movie(movie_id: int, movie: Movie):
             return {"message": "Movie not found"}
 
     return {"message": "Movie updated successfully"}
+
+@app.delete("/movies/{movie_id}")
+def delete_movie(movie_id: int):
+
+    with engine.begin() as conn:
+
+        result = conn.execute(
+            text("""
+                DELETE FROM movies
+                WHERE id = :id
+            """),
+            {"id": movie_id}
+        )
+
+        if result.rowcount == 0:
+            return {"message": "Movie not found"}
+
+    return {"message": "Movie deleted successfully"}
